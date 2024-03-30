e0e97babe4852bd8f6170b80b5b6d8cb738275c4
pedrobealves <pedrobealves@gmail.com>
2024-03-30 16:54:29 -0300

    Add deploy script to angular.json and update package.json dependencies. Add home-avatar and home-contact components to home-panel-left. Update styles.scss with new styles.

---
+++ src/app/features/home/components/home-avatar/home-avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-avatar.component.html',
+  styleUrl: './home-avatar.component.scss'
+})
+export class HomeAvatarComponent {
+
+}
--- src/app/features/home/components/home-panel-left/home-panel-left.component.ts
+++ src/app/features/home/components/home-panel-left/home-panel-left.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { HomeHeaderComponent } from '../home-header/home-header.component'
+import { HomeAvatarComponent } from '../home-avatar/home-avatar.component'
+import { HomeContactComponent } from '../home-contact/home-contact.component'
 
 @Component({
   selector: 'app-home-panel-left',
   standalone: true,
-  imports: [HomeHeaderComponent],
+  imports: [HomeHeaderComponent, HomeAvatarComponent, HomeContactComponent],
   templateUrl: './home-panel-left.component.html',
   styleUrl: './home-panel-left.component.scss'
 })
