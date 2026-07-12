9619573b9a5b5429d7e97ce26400b98247f45690
pedrobealves <pedrobealves@gmail.com>
2026-07-11 22:14:46 -0300

    fix(skills): use icon slug overrides for cdn urls

--- src/app/features/home/components/home-skills-wall/home-skills-wall.component.ts
+++ src/app/features/home/components/home-skills-wall/home-skills-wall.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject } from '@angular/core'
+import { Skill } from '../../models/skill.model'
 import { DataService } from '../../services/data.service'
+
+const SKILL_ICON_CDN = 'https://cdn.simpleicons.org'
+const SKILL_ICON_COLOR = '62FD85'
 
 @Component({
   selector: 'app-home-skills-wall',
@@ -9,4 +13,8 @@ import { DataService } from '../../services/data.service'
 })
 export class HomeSkillsWallComponent {
   protected skills = inject(DataService).skills
+
+  protected iconUrl(skill: Skill): string {
+    return `${SKILL_ICON_CDN}/${skill.logo ?? skill.name}/${SKILL_ICON_COLOR}`
+  }
 }
