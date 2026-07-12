0fcf63e6afdf30d515aa2c8d4c11d41efed5c7b7
pedrobealves <pedrobealves@gmail.com>
2026-07-12 03:16:52 -0300

    perf: switch to zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { provideAngularSvgIcon } from 'angular-svg-icon'
 
@@ -7,6 +7,7 @@ import { provideHttpClient, withXhr } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withXhr()),
     provideAngularSvgIcon()
