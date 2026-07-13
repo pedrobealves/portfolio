d9407680da9bb4ae05c54d33bc1dbf009b38fa35
pedrobealves <pedrobealves@gmail.com>
2026-07-12 21:23:52 -0300

    revert(header): remove subtitle and language switcher, restore dots

--- src/app/home/components/home-header/home-header.ts
+++ src/app/home/components/home-header/home-header.ts
@@ -1,10 +1,5 @@
-import { ChangeDetectionStrategy, Component, LOCALE_ID, inject } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { PortfolioContent } from '../../portfolio-content'
-
-const OTHER_LOCALE = {
-  'pt-BR': { href: '/en/', label: 'EN' },
-  en: { href: '/', label: 'PT' }
-} as const
 
 @Component({
   selector: 'app-home-header',
@@ -15,6 +10,4 @@ const OTHER_LOCALE = {
 })
 export class HomeHeader {
   protected profile = inject(PortfolioContent).profile
-  protected readonly otherLocale =
-    OTHER_LOCALE[inject(LOCALE_ID) as keyof typeof OTHER_LOCALE] ?? OTHER_LOCALE['pt-BR']
 }
