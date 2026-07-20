9409c00194b760e486920103b4d75c969ce00f42
pedrobealves <pedrobealves@gmail.com>
2026-07-19 23:39:13 -0300

    feat(projects): add circulogi and open project details in a dialog

--- src/app/home/components/home-carousel/home-carousel.ts
+++ src/app/home/components/home-carousel/home-carousel.ts
@@ -3,14 +3,33 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
+  signal,
   viewChild
 } from '@angular/core'
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { faBrandGithub } from '@ng-icons/font-awesome/brands'
+import {
+  faSolidArrowUpRightFromSquare,
+  faSolidImage
+} from '@ng-icons/font-awesome/solid'
 import { EmblaCarouselDirective } from 'embla-carousel-angular'
+import { Button } from '../../../shared/button/button'
+import { DetailDialog } from '../../../shared/detail-dialog/detail-dialog'
+import { Project } from '../../models/project'
 import { PortfolioContent } from '../../portfolio-content'
 
 @Component({
     selector: 'app-home-carousel',
-    imports: [EmblaCarouselDirective, NgOptimizedImage],
+    imports: [
+        EmblaCarouselDirective,
+        NgOptimizedImage,
+        DetailDialog,
+        Button,
+        NgIconComponent
+    ],
+    viewProviders: [
+        provideIcons({ faSolidImage, faSolidArrowUpRightFromSquare, faBrandGithub })
+    ],
     templateUrl: './home-carousel.html',
     styleUrl: './home-carousel.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -20,6 +39,7 @@ export class HomeCarousel {
 
   protected projects = inject(PortfolioContent).projects
   protected options = { loop: true }
+  protected selected = signal<Project | null>(null)
 
   scrollNext() {
     this.emblaRef()?.emblaApi?.scrollNext()
